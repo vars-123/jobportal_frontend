a8637b5133cdf5c643f57a5a2e06578086d252cf
vars-123 <varshithatl731@gmail.com>
2025-11-06 01:13:16 +0530

    Jenkinsfile

---
+++ dokploy.py
@@ -0,0 +1,30 @@
+# pip install requests
+
+import requests
+
+API_URL = "http://3.138.202.184:3000/api/project.all"
+api_key = "WDTngTTsEFzHtWEUUuWsKcJvcETeVyvqnHDfihhEuffMqvYWzLDsNIftKIDwKaYU"
+app_name = "job-portal-frontend-mgyetz"
+
+
+
+def get_application_id():
+   """Fetch Application ID"""
+   headers = {
+       "accept": "application/json",
+       "x-api-key": api_key,
+   }
+   req = requests.get(API_URL, headers=headers, timeout=30)
+   req.raise_for_status()
+   data = req.json()
+   # Safely iterate and find the requested application by name
+   for project in data or []:
+       environments = project.get("environments", [])
+       for env in environments:
+           applications = env.get("applications", [])
+           for app in applications:
+               if app.get("appName") == app_name:
+                   print(app.get("applicationId"))
+   return req.content
+
+get_application_id()
